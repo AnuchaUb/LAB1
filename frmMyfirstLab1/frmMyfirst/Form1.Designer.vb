3641df54c221ef943ac0a8ebe9dec7305bccfbee
anuchaub <mranczmsdz@hotmail.com>
2018-11-27 20:44:31 +0700

    VBLAB1

---
+++ frmMyfirstLab1/frmMyfirst/Form1.Designer.vb
@@ -0,0 +1,181 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtSalary = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lblSarary = New System.Windows.Forms.Label()
+        Me.lblTax = New System.Windows.Forms.Label()
+        Me.lblTotal = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.btnCal = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(18, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(57, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "เงินเดือน :"
+        '
+        'txtSalary
+        '
+        Me.txtSalary.Location = New System.Drawing.Point(100, 21)
+        Me.txtSalary.Name = "txtSalary"
+        Me.txtSalary.Size = New System.Drawing.Size(100, 20)
+        Me.txtSalary.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(18, 57)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(64, 13)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "รายได้ต่อปี :"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(18, 88)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(80, 13)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "อัตราภาษี (5%) :"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(18, 118)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(63, 13)
+        Me.Label4.TabIndex = 0
+        Me.Label4.Text = "รายได้สุทธิ :"
+        '
+        'lblSarary
+        '
+        Me.lblSarary.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.lblSarary.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.lblSarary.Location = New System.Drawing.Point(104, 57)
+        Me.lblSarary.Name = "lblSarary"
+        Me.lblSarary.Size = New System.Drawing.Size(64, 13)
+        Me.lblSarary.TabIndex = 0
+        '
+        'lblTax
+        '
+        Me.lblTax.BackColor = System.Drawing.Color.DarkGoldenrod
+        Me.lblTax.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.lblTax.Location = New System.Drawing.Point(104, 88)
+        Me.lblTax.Name = "lblTax"
+        Me.lblTax.Size = New System.Drawing.Size(64, 13)
+        Me.lblTax.TabIndex = 0
+        '
+        'lblTotal
+        '
+        Me.lblTotal.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.lblTotal.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.lblTotal.Location = New System.Drawing.Point(104, 118)
+        Me.lblTotal.Name = "lblTotal"
+        Me.lblTotal.Size = New System.Drawing.Size(64, 13)
+        Me.lblTotal.TabIndex = 0
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(174, 57)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(26, 13)
+        Me.Label8.TabIndex = 0
+        Me.Label8.Text = "บาท"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(174, 88)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(26, 13)
+        Me.Label9.TabIndex = 0
+        Me.Label9.Text = "บาท"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(174, 118)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(26, 13)
+        Me.Label10.TabIndex = 0
+        Me.Label10.Text = "บาท"
+        '
+        'btnCal
+        '
+        Me.btnCal.Location = New System.Drawing.Point(93, 155)
+        Me.btnCal.Name = "btnCal"
+        Me.btnCal.Size = New System.Drawing.Size(75, 23)
+        Me.btnCal.TabIndex = 2
+        Me.btnCal.Text = "คำนวณ"
+        Me.btnCal.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 208)
+        Me.Controls.Add(Me.btnCal)
+        Me.Controls.Add(Me.txtSalary)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.lblTotal)
+        Me.Controls.Add(Me.lblTax)
+        Me.Controls.Add(Me.lblSarary)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtSalary As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents lblSarary As Label
+    Friend WithEvents lblTax As Label
+    Friend WithEvents lblTotal As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents btnCal As Button
+End Class
---
+++ frmMyfirstLab1/frmMyfirst/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form1
+    Private Sub btnCal_Click(sender As Object, e As EventArgs) Handles btnCal.Click
+        lblSarary.Text = Val(txtSalary.Text) * 12
+        lblTax.Text = Val(lblSarary.Text) * 0.05
+        lblTotal.Text = Val(lblSarary.Text) - Val(lblTax.Text)
+    End Sub
+End Class
