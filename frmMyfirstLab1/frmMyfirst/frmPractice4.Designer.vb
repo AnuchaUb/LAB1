55623a7c3f50436911d477d6cda94b84305fc20b
anuchaub <45188884+AnuchaUb@users.noreply.github.com>
2018-12-23 00:37:14 +0700

    Practice1,2,4,5

---
+++ frmMyfirstLab1/frmMyfirst/frmPractice4.Designer.vb
@@ -0,0 +1,216 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmPractice4
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.lblProId = New System.Windows.Forms.Label()
+        Me.txtProAmount = New System.Windows.Forms.TextBox()
+        Me.radProCash = New System.Windows.Forms.RadioButton()
+        Me.radProCredit = New System.Windows.Forms.RadioButton()
+        Me.lblProName = New System.Windows.Forms.Label()
+        Me.lblProPrice = New System.Windows.Forms.Label()
+        Me.lblProTotal = New System.Windows.Forms.Label()
+        Me.lblProNet = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(70, 43)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(61, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "รหัสสินค้า"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(70, 79)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 17)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "ชื่อสินค้า"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(70, 109)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(82, 17)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "ราคาต่อหน่วย"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(70, 144)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(46, 17)
+        Me.Label4.TabIndex = 0
+        Me.Label4.Text = "จำนวน"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(70, 179)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(48, 17)
+        Me.Label5.TabIndex = 0
+        Me.Label5.Text = "เป็นเงิน"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(70, 219)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(75, 17)
+        Me.Label6.TabIndex = 0
+        Me.Label6.Text = "การชำระเงิน"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(70, 257)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(57, 17)
+        Me.Label7.TabIndex = 0
+        Me.Label7.Text = "ยอดชำระ"
+        '
+        'lblProId
+        '
+        Me.lblProId.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblProId.Location = New System.Drawing.Point(173, 42)
+        Me.lblProId.Name = "lblProId"
+        Me.lblProId.Size = New System.Drawing.Size(239, 17)
+        Me.lblProId.TabIndex = 0
+        '
+        'txtProAmount
+        '
+        Me.txtProAmount.Location = New System.Drawing.Point(173, 141)
+        Me.txtProAmount.Name = "txtProAmount"
+        Me.txtProAmount.Size = New System.Drawing.Size(239, 22)
+        Me.txtProAmount.TabIndex = 1
+        '
+        'radProCash
+        '
+        Me.radProCash.AutoSize = True
+        Me.radProCash.Location = New System.Drawing.Point(176, 217)
+        Me.radProCash.Name = "radProCash"
+        Me.radProCash.Size = New System.Drawing.Size(63, 21)
+        Me.radProCash.TabIndex = 2
+        Me.radProCash.TabStop = True
+        Me.radProCash.Text = "เงินสด"
+        Me.radProCash.UseVisualStyleBackColor = True
+        '
+        'radProCredit
+        '
+        Me.radProCredit.AutoSize = True
+        Me.radProCredit.Location = New System.Drawing.Point(273, 217)
+        Me.radProCredit.Name = "radProCredit"
+        Me.radProCredit.Size = New System.Drawing.Size(67, 21)
+        Me.radProCredit.TabIndex = 2
+        Me.radProCredit.TabStop = True
+        Me.radProCredit.Text = "เงินเชื่อ"
+        Me.radProCredit.UseVisualStyleBackColor = True
+        '
+        'lblProName
+        '
+        Me.lblProName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblProName.Location = New System.Drawing.Point(173, 78)
+        Me.lblProName.Name = "lblProName"
+        Me.lblProName.Size = New System.Drawing.Size(239, 17)
+        Me.lblProName.TabIndex = 0
+        '
+        'lblProPrice
+        '
+        Me.lblProPrice.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblProPrice.Location = New System.Drawing.Point(173, 108)
+        Me.lblProPrice.Name = "lblProPrice"
+        Me.lblProPrice.Size = New System.Drawing.Size(239, 17)
+        Me.lblProPrice.TabIndex = 0
+        '
+        'lblProTotal
+        '
+        Me.lblProTotal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblProTotal.Location = New System.Drawing.Point(173, 178)
+        Me.lblProTotal.Name = "lblProTotal"
+        Me.lblProTotal.Size = New System.Drawing.Size(239, 17)
+        Me.lblProTotal.TabIndex = 0
+        '
+        'lblProNet
+        '
+        Me.lblProNet.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblProNet.Location = New System.Drawing.Point(173, 256)
+        Me.lblProNet.Name = "lblProNet"
+        Me.lblProNet.Size = New System.Drawing.Size(239, 17)
+        Me.lblProNet.TabIndex = 0
+        '
+        'frmPractice4
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(547, 351)
+        Me.Controls.Add(Me.radProCredit)
+        Me.Controls.Add(Me.radProCash)
+        Me.Controls.Add(Me.txtProAmount)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.lblProNet)
+        Me.Controls.Add(Me.lblProTotal)
+        Me.Controls.Add(Me.lblProPrice)
+        Me.Controls.Add(Me.lblProName)
+        Me.Controls.Add(Me.lblProId)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmPractice4"
+        Me.Text = "แบบฝึกปฏิบัติสัปดาห์ที่ 4"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents lblProId As Label
+    Friend WithEvents txtProAmount As TextBox
+    Friend WithEvents radProCash As RadioButton
+    Friend WithEvents radProCredit As RadioButton
+    Friend WithEvents lblProName As Label
+    Friend WithEvents lblProPrice As Label
+    Friend WithEvents lblProTotal As Label
+    Friend WithEvents lblProNet As Label
+End Class
