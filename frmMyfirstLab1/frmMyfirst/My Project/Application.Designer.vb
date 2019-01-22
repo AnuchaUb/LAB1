e5b59bbf763c611e16a2a3fc1c1b177e5c2ee4f9
anucha <mranczmsdz@hotmail.com>
2019-01-22 16:15:50 +0700

    1/22/19
    AllNew

--- frmMyfirstLab1/frmMyfirst/My Project/Application.Designer.vb
+++ frmMyfirstLab1/frmMyfirst/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.frmMyfirst.frmLab7
+            Me.MainForm = Global.frmMyfirst.frmLab10
         End Sub
     End Class
 End Namespace
